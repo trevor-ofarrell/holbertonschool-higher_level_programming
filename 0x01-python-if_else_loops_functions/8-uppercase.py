83efd406ad0cb8e3fddcf7e422f769de4cdc9a27
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-10 23:27:35 +0000

    print sentance in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def islower(c):
+    if ord(c) in range(97, 123):
+        return True
+    return False
+
+
+def uppercase(str):
+    for i in str:
+        if islower(i):
+            print("{:c}".format(ord(i) - 32), end="")
+        else:
+            print("{:c}".format(ord(i)), end="")
