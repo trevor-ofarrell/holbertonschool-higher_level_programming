738d1278ef399c876311dfbe0d68ef435b13168c
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-11 01:26:38 +0000

    removing islower function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    ret = ''
     for i in str:
         if ord(i) in range(97, 123):
-            c = 32
+            ret = ret + chr(ord(i) - 32)
         else:
-            c = 0
-        print('{:c}'.format(ord(i) - c), end="")
-    print(" ")
+            ret = ret + i
+    print("{:s}".format(ret))
