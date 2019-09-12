d8b350506b84d922b6a2df43df1b6466ddc15ae8
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-12 23:57:05 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import add_0 as add
-a = 1
-b = 2
-print(str(a), "+", str(b), "=", "{:d}".format(add.add(a, b)))
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print(str(a), "+", str(b), "=", "{:d}".format(add.add(a, b)))
