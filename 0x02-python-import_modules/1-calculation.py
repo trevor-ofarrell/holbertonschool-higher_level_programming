b6fb58def47e83b1c7355294f70a27cdccf0282f
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-12 23:56:07 +0000

    adding name condition

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import calculator_1 as calc
-a = 10
-b = 5
-print(a, "+", b, "=", "{:d}".format(calc.add(a, b)))
-print(a, "-", b, "=", "{:d}".format(calc.sub(a, b)))
-print(a, "*", b, "=", "{:d}".format(calc.mul(a, b)))
-print(a, "/", b, "=", "{:d}".format(calc.div(a, b)))
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print(a, "+", b, "=", "{:d}".format(calc.add(a, b)))
+    print(a, "-", b, "=", "{:d}".format(calc.sub(a, b)))
+    print(a, "*", b, "=", "{:d}".format(calc.mul(a, b)))
+    print(a, "/", b, "=", "{:d}".format(calc.div(a, b)))
