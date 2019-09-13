745fe65d39d007e959d61e579f242b6b24d89f18
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-13 08:45:17 +0000

    simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,14 +3,13 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    a = sys.argv[1]
-    b = sys.argv[3]
+    op = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-    op = sys.argv[2]
 
     if op == '\*':
         result = mul(int(a), int(b))
