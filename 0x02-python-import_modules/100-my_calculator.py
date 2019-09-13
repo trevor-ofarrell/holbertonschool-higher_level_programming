9843907d51f64da1e112e19a9182ff47da0be9b0
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-13 08:34:38 +0000

    simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,19 +4,20 @@ if __name__ == "__main__":
     import sys
     a = sys.argv[1]
     b = sys.argv[3]
-    if len(sys.argv) != 4:
+    if len(sys.argv) < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     op = sys.argv[2]
+
     if op == '*':
         result = mul(int(a), int(b))
     elif op == '-':
         result = sub(int(a), int(b))
     elif op == "+":
-        result = add(a, b)
+        result = add(int(a), int(b))
     elif op == '/':
         result = div(int(a), int(b))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print(a, op, b, '=', result)
+    print(int(a), op, int(b), '=', int(result))
