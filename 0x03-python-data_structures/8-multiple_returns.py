1c714e00f9630cdd7be36c7b78e3b03601f001cf
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-18 03:04:41 +0000

    return tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[:1] = None
+        return (0, None)
     tup = (len(sentence), sentence[:1])
     return tup
