e724558f4cdadfd69be5f793e82d9123d6522e05
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-18 06:49:21 +0000

    refactoring

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
         for i in rows:
-            print("{}".format(i), end='')
+            print("{}".format(i), end="")
             if i != rows[-1]:
-                print('', end=" ")
-        print()
+                print("", end=" ")
+        print("")
