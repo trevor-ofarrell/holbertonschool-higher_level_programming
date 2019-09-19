0e6db8689d5f6719203387d1481d3ce2a23b2df2
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-19 22:34:51 +0000

    add uniq integers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq = []
+    for x in my_list:
+        if x not in uniq:
+            uniq.append(x)
+    for x in uniq:
+        sm = sum(uniq)
+    return sm
