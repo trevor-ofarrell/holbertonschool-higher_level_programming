aeafd05e3fe4c7bd646622549f5244be125d97a7
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-19 22:44:31 +0000

    add uniq integers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,6 @@
 def uniq_add(my_list=[]):
     nlist = set(my_list)
     uniq = (list(nlist))
-    if not my_list:
-        return None
-    for x in uniq:
-        return sum(uniq)
+    if my_list is not None:
+        for x in uniq:
+            return sum(uniq)
