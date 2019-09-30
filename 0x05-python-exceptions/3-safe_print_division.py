d37a824a875c0eb053b75ec6778bdf2ce9f42ed5
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-30 19:58:19 +0000

    optimizing

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,11 +3,8 @@ def safe_print_division(a, b):
     ssum = 0
     try:
         ssum = a / b
-    except (ZeroDivisionError):
-        return None
+    except:
+        ssum = None
     finally:
-        if ssum == 0:
-            print("Inside result: ""{}".format(None))
-            return None
-        print("Inside result: ""{:.1f}".format(ssum))
-        return ssum
+        print("Inside result: ""{}".format(ssum))
+    return ssum
