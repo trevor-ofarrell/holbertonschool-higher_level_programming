15430b2671e7773d5c13707259651f5cf54e8f1d
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-30 19:04:20 +0000

    initial commit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int) == True:
+        if isinstance(value, int) is True:
             print("{:d}".format(value))
             return True
         else:
