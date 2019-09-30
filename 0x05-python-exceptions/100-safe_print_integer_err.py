01063a18bf8d0fa222a65c4243a7b39587c3459e
Trevor-Ofarrell <744@holbertonschool.com>
2019-09-30 20:47:25 +0000

    print list with error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,7 +8,7 @@ def safe_print_integer_err(value):
         if isinstance(value, int) is True:
             print("{:d}".format(value))
             return True
-        return False
     except Exception as e:
         sys.stderr.write("Exception: ", traceback.print_exc())
         print(e, file=sys.stderr)
+        return False
