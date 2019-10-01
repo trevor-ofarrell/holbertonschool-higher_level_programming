2be7baca12f8fbfd84bcf1f5f4b81d7d7d00fdb7
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-01 23:17:23 +0000

    adding setters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,32 +5,32 @@ class Square:
     def __init__(self, size=0):
 
         """init method
+
         Attributes:
-        attr1 (:obj - size) size of square
+
+           attr1 (size) - size of square
+
         Raises:
-        ValueError: If size is negitive
-        TypeError: If size is not an int"""
+
+           ValueError:  If size is negitive
+           TypeError:  If size is not an int"""
 
         self.size = size
-        if size < 0:
-            try:
-                raise ValueError("size must be >= 0")
-            except ValueError:
-                raise
 
+    @property
     def size(self):
         """property getter"""
         return self.__size
 
+    @size.setter
     def size(self, value):
         """property setter"""
+        if isinstance(value, str) is True:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
         """Public instance method to return area of square"""
-        if isinstance(self.size, str) is True:
-            try:
-                raise TypeError("size must be an integer")
-            except TypeError:
-                raise
-        return self.size ** 2
+        return self.__size ** 2
