9831eb66570c62c5bce7010df7a9382e55a18187
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-01 06:04:11 +0000

    fixing print stateent

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     import sys
-    import traceback
     try:
-        if isinstance(value, int) is True:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except Exception as e:
-        print("Exception: "+e, file=sys.stderr)
+        print("Exception: {}\n".format(e), file=sys.stderr, end="")
         return False
