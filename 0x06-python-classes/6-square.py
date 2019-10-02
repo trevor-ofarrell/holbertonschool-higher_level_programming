d8eae453dd0d3437a23468a0dd5bce74b3aaf209
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-02 06:34:52 +0000

    adding check if values are ints

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,13 +44,13 @@ class Square:
 
         if isinstance(value, tuple) is False:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
         if isinstance(value[0], int) is False:
             raise TypeError("position must be a tuple of 2 positive integers")
         if isinstance(value[1], int) is False:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
