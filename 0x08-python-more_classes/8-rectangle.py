2ba5b2ae94deef727db17d5d8465a035f29c8369
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-08 01:50:33 +0000

    initial commit

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -97,6 +97,7 @@ class Rectangle:
 
         type(self).number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
 
         """static method to return biggest rectangle based
