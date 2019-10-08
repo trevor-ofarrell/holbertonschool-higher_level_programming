94885fbfb94ef2912997dcf3886edbc07f1cfe14
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-08 07:21:31 +0000

    adding doc

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -7,6 +7,7 @@ if len(argv) != 2:
 
 try:
     N = int(argv[1])
+
 except:
     print("N must be a number")
     exit(1)
@@ -17,17 +18,31 @@ if N < 4:
 
 
 def solve(N, i, a, b, c):
+
+    """function to determine squares where a queen can be placed without
+       attacking another"""
+
     if i < int(N):
         for j in range(N):
+
             if j not in a and i+j not in b and i-j not in c:
+
                 for solution in solve(N, i+1, a+[j], b+[i+j], c+[i-j]):
+
                     yield solution
     else:
         yield a
 
 for solution in solve(N, 0, [], [], []):
+
+    """function to print representaion of squares that are valid"""
+
     e1 = enumerate(solution)
+
     print("[", end='')
+
     for ele in e1:
+
         print("{}".format(list(ele)), end='')
+
     print("]")
