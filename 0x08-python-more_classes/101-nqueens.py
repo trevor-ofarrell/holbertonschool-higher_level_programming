3543d9ceff4f4342cd3aa57376b36684a5262381
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-08 07:07:23 +0000

    adding conditions

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
 from sys import argv
+
 if len(argv) != 2:
     print("Usage: nqueens N")
     exit(1)
+
 N = int(argv[1])
+
+if isinstance(N, str):
+    print("N must be a number")
+    exit(1)
+
+N = int(argv[1])
+
+if not isinstance(N, int):
+    print("N must be a number")
+    exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
+
 count = 0
+
 
 def solve(N, i, a, b, c):
     if i < int(N):
