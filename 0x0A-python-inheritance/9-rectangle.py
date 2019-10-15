f1971394aaf6829c8168b355aec66d208a3e84ad
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-15 23:42:59 +0000

    initial commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+
+    """class for rectangles derived from the base geometry superclass"""
+
+    def __init__(self, width, height):
+
+        """init method"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+
+        """returns area of rectangle"""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+
+        """string representation of the rectangle"""
+
+        return "{} {}/{}".format("[Rectangle]", self.__width, self.__height)
