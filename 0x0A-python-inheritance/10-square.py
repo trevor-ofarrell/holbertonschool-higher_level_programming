af6ba3c2a577f323f35af63807369292918645be
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-16 00:14:57 +0000

    initial commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+
+        """init method"""
+
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+        super().__init__(size, size)
+
+    def area(self):
+
+        """method to calculate area of square"""
+        return self.__size * self.__size
