7f2e2e2e595d5c001b48bf24614f80cc9bf5fb8f
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-16 22:49:28 +0000

    update

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -4,5 +4,6 @@ import json
 
 def load_from_json_file(filename):
 
-    with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f)
+    with open(filename, mode="r", encoding="utf-8") as file:
+
+        return json.load(file)
