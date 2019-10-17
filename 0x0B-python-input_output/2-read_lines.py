76ae7c41bc33457b1bb73104b0092b5bf055f8b8
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-17 06:04:01 +0000

    removing newlines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-
-    count = 0
-
-    with open(filename, mode='r', encoding='utf-8') as file:
-
-        for line in file:
-
-            count += 1
-
-        return count
-
-
 def read_lines(filename="", nb_lines=0):
 
+    c = 0
+
     with open(filename, mode='r', encoding='utf-8') as file:
 
-        if nb_lines <= 0 or nb_lines > number_of_lines(filename):
+        if nb_lines <= 0:
 
-            print(str(file.read()))
+            print(file.read(), end='')
 
         else:
 
-            head = [next(file) for i in range(nb_lines)]
+            for line in file:
 
-            print(str(head))
+                if c < nb_lines:
+
+                    print(line, end='')
+
+                c += 1
