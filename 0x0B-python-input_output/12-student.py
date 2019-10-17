03985c0063006eef4be57612698201ee36f536d8
Trevor-Ofarrell <744@holbertonschool.com>
2019-10-17 04:29:02 +0000

    update

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+class Student():
+
+    """class to describe students"""
+
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+
+        """init method"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        """retieves a dictionary rep of a student instance"""
+
+        stu = {}
+
+        if not attrs:
+
+            return self.__dict__
+
+        for key, value in self.__dict__.items():
+
+            if key in attrs and all(isinstance(key, str) for key in attrs):
+
+                stu[key] = value
+
+        return stu
