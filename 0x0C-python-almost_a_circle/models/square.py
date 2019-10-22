586041ec71ef6c78007b2f4ea5a381ada1b7f823
trevor-ofarrell <744@holbertonschool.com>
2019-10-22 21:41:25 +0000

    modifying update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -42,13 +42,13 @@ class Square(Rectangle):
 
         if args:
 
-            if len(args) >= 0:
+            if len(args) > 0:
                 setattr(self, 'id', args[0])
-            if len(args) == 2:
+            if len(args) >= 2:
                 setattr(self, 'size', args[1])
-            if len(args) == 3:
+            if len(args) >= 3:
                 setattr(self, 'x', args[2])
-            if len(args) == 4:
+            if len(args) >= 4:
                 setattr(self, 'y', args[3])
 
         else:
