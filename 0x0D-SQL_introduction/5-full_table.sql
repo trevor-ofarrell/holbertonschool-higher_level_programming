a2be7b59ccdd8a8a712e55a557910fc864f0f2cd
root <744@holbertonschool.com>
2019-11-26 05:10:25 +0000

    adding 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- describe table without describe statement
-select * 
-  from information_schema.columns 
- where table_name = 'first_table';
+SELECT * 
+  FROM information_schema.columns 
+ WHERE table_name = 'first_table';
