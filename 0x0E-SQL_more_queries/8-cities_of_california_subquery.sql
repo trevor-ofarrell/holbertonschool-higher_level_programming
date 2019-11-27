5411b7ca41a2da72fc5679ea49b7896b30a87ada
Trevor <trevor-ofarrell>
2019-11-26 20:58:53 -0800

    refactor

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- query to find cities in california
 SELECT id, name
-FROM DATABASE hbtn_0d_usa
-WHERE hbtn_0d_usa.states.state_id =
+FROM hbtn_0d_usa.cities
+WHERE state_id =
      (SELECT id
      FROM hbtn_0d_usa.states
      WHERE name = "California")
-ORDER BY hbtn_od_usa.cities.id ASC;
+ORDER BY id ASC;
