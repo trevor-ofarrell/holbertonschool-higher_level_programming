a61055beb82673919f9902f80801ed0ef12e5743
trevor-ofarrell <744@holbertonschool.com>
2019-11-27 03:39:45 +0000

    initial commit

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create cities table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+       state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY (id)
+);
