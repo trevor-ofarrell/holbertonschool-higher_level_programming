6d23894631b928b6ccd15f1b9cd289bea1726e78
trevor-ofarrell <744@holbertonschool.com>
2019-11-27 03:19:14 +0000

    initial commit

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create db and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
        id INT UNIQUE AUTO_INCREMENT NOT NULL,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY (id)
