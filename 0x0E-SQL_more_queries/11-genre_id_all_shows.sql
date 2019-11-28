6a4dc6de7df45a3b72a2dde094639d342f6fabea
Trevor <trevor-ofarrell>
2019-11-27 21:44:19 -0800

    adding task 11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,4 @@
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
