83dd71a649d91c490868cfa020321adda9dc02ca
trevor-ofarrell <744@holbertonschool.com>
2019-12-14 22:10:50 +0000

    adding readme

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,14 @@
 import MySQLdb
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sys import argv
-Base = declarative_base()
-Session = sessionmaker()
-enginestr = argv[1] + ':' + argv[2] + '@' + 'localhost' + ':'
-engine = sqlalchemy.create_engine("{}{}/{}".format(enginestr, 3306, argv[3]))
-Session.configure(bind=engine)
-session = Session()
-for instance in session.query(state).order_by(states.id):
-    print(instance.name)
+if __name__ == "__main__":
+    Base = declarative_base()
+    Session = sessionmaker()
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states; ORDER BY state.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
