653660d6500d4103dac076d2312adfce95e28546
trevor-ofarrell <744@holbertonschool.com>
2019-12-14 22:16:40 +0000

    indenting

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,8 @@ from sys import argv
 if __name__ == "__main__":
     Base = declarative_base()
     Session = sessionmaker()
-    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states; ORDER BY states.id ASC;")
     rows = cur.fetchall()
