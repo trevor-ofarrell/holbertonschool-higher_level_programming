aa8faea23da4925a097d1caafe71fdbd756d80de
trevor-ofarrell <744@holbertonschool.com>
2019-12-14 22:33:03 +0000

    adding semicolon

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+        "SELECT * FROM states WHERE name LIKE 'N%'; ORDER BY states.id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
