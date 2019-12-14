6e78b4555c320628874de69c02ecd3e056ee3636
trevor-ofarrell <744@holbertonschool.com>
2019-12-14 23:01:35 +0000

    adding .format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,11 +8,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states; ORDER BY states.id ASC;")
+    query1 = "SELECT * FROM states WHERE name LIKE"
+    query2 = "ORDER BY states.id ASC;"
+    query = "{} '%{}%' {}".format(query1, argv[4], query2)
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
-    cur.close
-    db.close
+        print(row)
+    cur.close()
+    db.close()
