848f76601f379efbff4b9b6d0eb0d06e47b1c660
trevor-ofarrell <744@holbertonschool.com>
2019-12-14 22:43:30 +0000

    filter by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that lists all states with a name
+matching user input from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sqlalchemy
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states; ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close
+    db.close
