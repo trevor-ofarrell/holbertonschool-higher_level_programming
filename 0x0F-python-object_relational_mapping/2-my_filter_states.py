a75d8fe7207bfbbf07010befaca711b31e389850
trevor-ofarrell <744@holbertonschool.com>
2019-12-15 00:31:40 +0000

    fixing format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    query1 = "SELECT * FROM states WHERE name LIKE"
-    query2 = "ORDER BY states.id ASC;"
-    query = "{} '%{}%' {}".format(query1, argv[4], query2)
+    query = """SELECT * FROM states
+    WHERE name LIKE "{}" ORDER BY states.id ASC;"""
+    query = query.format(argv[4])
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
