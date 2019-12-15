1f4f0bd1b84f7761dd7f2e8b648971eb4d3ffd78
trevor-ofarrell <744@holbertonschool.com>
2019-12-15 23:29:23 +0000

    adding cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    query = "SELECT * FROM cities ORDER BY cities.id ASC;"
+    query = """SELECT cities.id, cities.name, states.name FROM
+    cities INNER JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC"""
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
