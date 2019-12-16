266a33917d345fc6178f1b206886424663538db8
trevor-ofarrell <744@holbertonschool.com>
2019-12-16 04:10:28 +0000

    adding fetch first

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script to list first states in db"""
+
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+from sys import argv
+
+if __name__ == "__main__":
+    Base = declarative_base()
+    Session = sessionmaker()
+    enginestr = 'mysql+mysqldb://'
+    engine = sqlalchemy.create_engine(
+        "{}{}:{}{}/{}".format(enginestr, argv[1], argv[2],
+                              '@localhost:3306', argv[3]))
+    Session.configure(bind=engine)
+    session = Session()
+    first = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(first.id, first.name))
