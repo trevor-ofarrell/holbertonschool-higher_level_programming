ad91f9b010b6b842b85105de0c04f950e3020fca
trevor-ofarrell <744@holbertonschool.com>
2019-12-16 03:58:21 +0000

    sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+from sys import argv
+
+if __name__ == "__main__":
+    Base = declarative_base()
+    Session = sessionmaker()
+    enginestr = 'mysql+mysqldb://'
+    engine = sqlalchemy.create_engine(
+        "{}{}:{}{}/{}".format(enginestr, argv[1], argv[2],
+                              '@localhost:3306', argv[3]))
+    Session.configure(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
