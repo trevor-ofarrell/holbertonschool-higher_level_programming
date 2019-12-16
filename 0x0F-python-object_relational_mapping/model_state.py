41058a8af43e0dc34d4bdadf97f593a67b3e7afc
trevor-ofarrell <744@holbertonschool.com>
2019-12-16 03:32:22 +0000

    adding state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,8 +12,5 @@ Base = declarative_base()
 class State(Base):
     """class for states in the US"""
     __tablename__ = 'states'
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         passwd='root', user='root')
-    cursor = db.cursor()
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
