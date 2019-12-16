d0227f82d6365e6e0a869708c72d2021fd649564
trevor-ofarrell <744@holbertonschool.com>
2019-12-16 03:29:56 +0000

    adding state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""k"""
+"""class definition of a State and
+an instance Base = declarative_base():"""
 import sqlite3
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
