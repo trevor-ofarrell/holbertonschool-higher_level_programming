4f50593439f6f64e6ef0a8f5c62f0bc9369f4525
Trevor O'Farrell <47125174+trevor-ofarrell@users.noreply.github.com>
2019-12-19 18:30:49 -0800

    Create relationship_city.py

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""cities class"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+import MySQLdb
+
+Base = declarative_base()
+
+
+class City(Base):
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    
