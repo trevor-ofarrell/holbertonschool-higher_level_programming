d476f46c3e708b5eedc895a288cfe1fda4a34b1a
trevor-ofarrell <744@holbertonschool.com>
2020-01-14 06:52:09 +0000

    adding starwars

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -3,8 +3,10 @@
 search request to the Star Wars API"""
 import requests
 import sys
-re = requests.get("https://swapi.co/api/people/?search={}".format(sys.argv[1]))
-data = re.json()
-print("{}{}".format("Nmber of results: ", data.get("count")))
-for name in data.get('results'):
-    print(name.get('name'))
+
+if __name__ == "__main__":
+    re = requests.get("https://swapi.co/api/people/?search={}".format(sys.argv[1]))
+    data = re.json()
+    print("{}{}".format("Nmber of results: ", data.get("count")))
+    for name in data.get('results'):
+        print(name.get('name'))
