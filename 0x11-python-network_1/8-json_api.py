82d3453ddc169f04bc4b38320077a534c88aad11
trevor-ofarrell <744@holbertonschool.com>
2020-01-14 04:21:59 +0000

    initial commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    if not sys.argv[1]:
+        q = ""
+    else:
+        q = sys.argv[1]
+    re = requests.post(url="http://0.0.0.0:5000/search_user", data={'q': q})
+    ret = re.json()
+    try:
+        if ret:
+            print("[{}] {}".format(ret.get('id'), ret.get('name')))
+        if not ret:
+            print("No result")
+    except:
+        print("Not a valid JSON")
