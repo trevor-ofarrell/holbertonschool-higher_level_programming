3227b2e46e68eba94fc2cc8fc1a16f08b7fb043f
trevor-ofarrell <744@holbertonschool.com>
2020-02-12 04:11:48 +0000

    store request body in file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
