f191ef2978fb9b65d260dfe2a34a185773f7a504
trevor-ofarrell <744@holbertonschool.com>
2020-02-12 00:27:28 +0000

    adding print for err

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, fileContent) => {
   if (err) {
-    console.log(err);
+    return console.log(err);
   }
   console.log(fileContent);
 });
